4147e72d5555ca907be5524a0e8c38c9b7e6e409
eTaylorOMS <evelyn.taylor@oldmainsolutions.com>
2024-07-25 12:47:09 -0400

    key-pair

--- Terraform/aws-ec2.tf
+++ Terraform/aws-ec2.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
   name = "tf-test"
 
   instance_type          = "t2.micro"
-  key_name               = "test"
+  key_name               = module.key_pair.key_pair_name
   monitoring             = true
   vpc_security_group_ids = [module.web_server_sg.security_group_id]
   subnet_id              = module.vpc.private_subnets[0]
