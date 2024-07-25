0c969200d49fbce594e7dd4d0393fc962e481407
gp-davidhardy <david.hardy@greenphire.com>
2024-07-25 11:53:20 -0400

    vpc defaults / vars

---
+++ Terraform/aws-vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source                   = "terraform-aws-modules/vpc/aws"
+  version                  = "2.55.0"
+  name                     = "eve-vpc"
+  cidr                     = "10.0.1.0/24"
+  azs                      = ["us-east-1a", "us-east-1b"]
+  private_subnets          = ["10.0.1.0/26", "10.0.1.64/26"]
+  public_subnets           = ["10.0.1.128/26", "10.0.1.192/26"]
+  enable_ipv6              = false
+  enable_dns_hostnames     = true
+  enable_dns_support       = true
+  enable_dhcp_options      = true
+  create_igw               = true
+  map_public_ip_on_launch	 = false
+}
